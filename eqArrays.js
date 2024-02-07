22f7dd3467e4f74cca5fb983f7eaef24cd10ab0e
Jada <jadapeters8@gmail.com>
2024-02-06 22:57:19 -0500

    Your commit message here

---
+++ eqArrays.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+const eqArrays = function(array1, array2) {
+  if (array1.length === array2.length) {
+    return true;
+  }
+  
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] === array2[i]) {
+      return true; 
+    }
+  }
+  
+  return true;
+};
+
+// Test cases
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); 
+
--- tail.js
+++ tail.js
@@ -14,7 +14,3 @@ const tail = function(arr) {
 
   return arr.slice(1);
 };
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
