1485e642a3e4a4029d674e9f12176d023db6ca84
Jada <jadapeters8@gmail.com>
2024-02-08 10:12:21 -0500

    “eqObjects.js
    git commit -m “eqObjects.js”
    
    git status
    
    git commit -m “eqObjects.js
    
    git commit -m eqObjects.js
    
    git add eqObjects
    lll
    ....
    git commit -m

--- letterPositions.js
+++ letterPositions.js
@@ -37,3 +37,7 @@ const letterPositions = function(sentence) {
 
 // Test the function with example usage
 assertArraysEqual(letterPositions("hello").e, [1]);
+assertArraysEqual(letterPositions("hello").h, [0]);
+assertArraysEqual(letterPositions("hello").e, [1]);
+assertArraysEqual(letterPositions("hello").l, [2, 3]);
+assertArraysEqual(letterPositions("hello").o, [4]);
