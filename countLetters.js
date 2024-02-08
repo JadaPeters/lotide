7cd2a79e547a544f41f1e9eb7e860849f88eebbe
Jada <jadapeters8@gmail.com>
2024-02-07 20:38:20 -0500

    Make a file called countLetters.js

---
+++ countLetters.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(word) {
+  const letterCounts = {};
+
+  for (const letter of word) {
+    if (letter !== ' ') {
+      if (letterCounts[letter]) {
+        letterCounts[letter] += 1;
+      } else { 
+        letterCounts[letter] = 1;
+      }
+    }
+  }
+
+  return letterCounts;
+};
+
+console.log(countLetters('LHL'));
