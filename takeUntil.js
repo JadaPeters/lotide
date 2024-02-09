8e9b58617040e52d7531fc0581ba8813f5c6a065
Jada <jadapeters8@gmail.com>
2024-02-08 22:15:28 -0500

    takeUntil.js

---
+++ takeUntil.js
@@ -0,0 +1,50 @@
+// It will take in two parameters as well:
+// The array to work with
+// The callback (which Lodash calls "predicate")
+// The function will return a "slice of the array with elements taken from the beginning." It should keep going until the callback/predicate returns a truthy value.
+// Implement takeUntil which will keep collecting items from a provided array until the callback provided returns a truthy value.
+
+
+const takeUntil = function(array, callback) {
+  const results = [];
+  for (const item of array) {
+    if (callback(item)) {
+      break;
+    }
+    results.push(item);
+  }
+  return results;
+};
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+
+
+const assertArraysEqual = function(array1, array2) {
+  const result = eqArrays(array1, array2);
+  if (result) {
+    console.log(`✅ Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${array1} !== ${array2}`);
+  }
+};
+
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] === array2[i]) {
+      return true; 
+    }
+  }
+  
+  return false;
+};
