41499124229fc74bb95e253b66cfc6e4cdeede89
Jada <jadapeters8@gmail.com>
2024-02-08 22:18:03 -0500

    takeUntil.js

--- takeUntil.js
+++ takeUntil.js
@@ -41,10 +41,10 @@ const eqArrays = function(array1, array2) {
   }
   
   for (let i = 0; i < array1.length; i++) {
-    if (array1[i] === array2[i]) {
-      return true; 
+    if (array1[i] !== array2[i]) {
+      return false; 
     }
   }
   
-  return false;
+  return true;
 };
