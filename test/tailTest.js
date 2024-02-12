4efd319d740005f2cd687a60d336eeaad72521c6
Jada <jadapeters8@gmail.com>
2024-02-12 16:24:23 -0500

    Refactored test cases

--- test/tailTest.js
+++ test/tailTest.js
@@ -2,5 +2,5 @@ const assertEqual = require('../assertEqual');
 const tail = require('../tail');
 
 // TEST CODE
-assertEqual(tail([1, 2, 3]).toString(), [2, 3].toString()); 
-assertEqual(tail(['apple', 'banana', 'cherry']).toString(), ['banana', 'cherry'].toString()); 
+assertEqual(tail([1, 2, 3])[0], 2);
+assertEqual(tail([1, 2, 3])[1], 3);
