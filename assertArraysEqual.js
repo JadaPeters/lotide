30ac1f1328c4cdb3545f20a0a933582fb795d688
Jada <jadapeters8@gmail.com>
2024-02-12 16:56:32 -0500

    assertArraysEqual Refactored

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,10 +1,7 @@
 const eqArrays = require('./eqArrays');
 
-
-
 const assertArraysEqual = function(array1, array2) {
-  const result = eqArrays(array1, array2);
-  if (result) {
+  if (eqArrays(array1, array2)) {
     console.log(`✅ Assertion Passed: ${array1} === ${array2}`);
   } else {
     console.log(`🛑 Assertion Failed: ${array1} !== ${array2}`);
@@ -12,4 +9,5 @@ const assertArraysEqual = function(array1, array2) {
 };
 
 module.exports = assertArraysEqual;
+
 
