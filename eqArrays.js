bd0a820839f079d21d8004d1668644429bbf3d3c
Jada <jadapeters8@gmail.com>
2024-02-12 16:34:07 -0500

    EqArrays Refactored

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+
 const eqArrays = function(array1, array2) {
   if (array1.length === array2.length) {
     return true;
@@ -19,6 +13,6 @@ const eqArrays = function(array1, array2) {
   return true;
 };
 
-// Test cases
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); 
+module.exports = eqArrays;
+
 
