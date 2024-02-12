351b290bbc7e8965a82dbce82dcf8570b857fe06
Jada <jadapeters8@gmail.com>
2024-02-12 16:00:48 -0500

    More test cases added

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -3,3 +3,6 @@ const assertEqual = require('../assertEqual');
 
 // TEST CODE
 assertEqual(22, 1);
+assertEqual(5, 10); 
+assertEqual("hello", "hello");
+assertEqual(5, 5);
--- test/headTest.js
+++ test/headTest.js
@@ -3,3 +3,5 @@ const head = require('../head');
 
 // TEST CODE
 assertEqual(head([5,6,7]), 5);
+assertEqual(head(['apple', 'banana', 'cherry']), 'apple');
+assertEqual(head([undefined, 2, 3]), undefined);
