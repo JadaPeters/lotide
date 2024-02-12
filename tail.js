f847750b2f9a8a9df757ccabde7873b3318e8ffe
Jada <jadapeters8@gmail.com>
2024-02-12 16:25:05 -0500

    Refactored

--- tail.js
+++ tail.js
@@ -1,12 +1,4 @@
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const tail = function(arr) {
   if (arr.length <= 1) {
     return []; 
@@ -14,3 +6,5 @@ const tail = function(arr) {
 
   return arr.slice(1);
 };
+
+module.exports = tail;
