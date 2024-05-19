857d11a33679056d66afe9a26cb26ee1f3e15841
Jada <jadapeters8@gmail.com>
2024-05-18 23:40:19 -0400

    EqArray Function modify

--- eqArrays.js
+++ eqArrays.js
@@ -5,7 +5,7 @@ const eqArrays = function(arr1, arr2) {
     return false
   }
   
-  for (let i = 1; i < arr1.length; i++) {
+  for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
       return false; 
     } 
@@ -24,4 +24,20 @@ const assertEqual = function(actual, expected) {
 };
 
 // Test Code
+// identical arrays
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+
+// arrays with different lengths
+assertEqual(eqArrays([1, 2, 3], [1, 2]), false); // => should PASS
+
+// arrays with same lengths but different elements
+assertEqual(eqArrays([1, 2, 3], [1, 2, 4]), false); // => should PASS
+
+// arrays with different lengths and different elements
+assertEqual(eqArrays([1, 2], [1, 2, 3]), false); // => should PASS
+
+// one empty array
+assertEqual(eqArrays([], [1, 2, 3]), false); // => should PASS
+
+// both arrays empty
+assertEqual(eqArrays([], []), true); // => should PASS
