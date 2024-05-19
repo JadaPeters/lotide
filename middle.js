fc4b47990642eeea35484f80635313e680a88cd9
Jada <jadapeters8@gmail.com>
2024-05-19 02:45:29 -0400

    middle Function created

---
+++ middle.js
@@ -0,0 +1,42 @@
+// eqArrays Function
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length){
+    return false;
+  }
+  
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false; 
+    }
+  }
+  return true;
+};
+
+// asserEqual Function
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+// FUNCTION IMPLEMENTATION
+// Implement middle which will take in an array and return the middle-most element(s) of the given array.
+const middle = function(array) {
+  let length = array.length;
+  const middleIndex = Math.floor(length / 2);
+    if (length <= 2) {
+      return [];
+    } else if (length % 2 === 0) {
+      return [array[middleIndex - 1], array[middleIndex]];
+    } else {
+      return [array[middleIndex]];
+    }
+  };
+
+
+// TEST CODE
+assertArraysEqual(middle([1, 2, 3]), [2]);
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
