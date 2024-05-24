730f7deb13e1ced7792efdbdf81bc086dc2eba47
Jada <jadapeters8@gmail.com>
2024-05-24 16:39:30 -0400

    countOnly.js modified

--- countOnly.js
+++ countOnly.js
@@ -19,15 +19,9 @@ const assertEqual = function(actual, expected) {
 
 const countOnly = function (allItems, itemsToCount) {
   const results = {};
-
   for (const item of allItems) {
-    console.log(item);
     if (itemsToCount[item]) {
-      if (results[item]) {
-        results[item] += 1;
-      } else {
-        results[item] = 1;
-      }
+      results[item] = (results[item] || 0) + 1;
     }
   }
 
