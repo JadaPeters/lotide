3de22ba1f199708c442b4ab995f42b9ad3622fd4
Jada <jadapeters8@gmail.com>
2024-05-24 17:48:43 -0400

    countLetters.js function created

---
+++ countLetters.js
@@ -0,0 +1,26 @@
+//  assertEqual function
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const countLetter = function(sentence){
+const letterCounts = {};
+
+for (const letter of sentence){
+    if (letterCounts[letter]){
+      letterCounts[letter] += 1;
+    } else {
+      letterCounts[letter] = 1;
+    
+  }
+}
+return letterCounts;
+};
+
+const result = countLetter('lighthouseinthehouse')
+console.log(result);
