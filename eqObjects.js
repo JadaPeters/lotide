58e1f53884e7f7c9ede25f7a39b4866dfebebec9
Jada <jadapeters8@gmail.com>
2024-05-25 18:55:56 -0400

    eqObjects.js function created

---
+++ eqObjects.js
@@ -0,0 +1,65 @@
+//  assertEqual function
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// eqArrays Function
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length){
+    return false;
+  }
+  
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false; 
+    }
+  }
+  return true;
+};
+
+
+
+
+// eqObjects
+// function eqObjects which will take in two objects and returns true or false, based on a perfect match.
+const eqObjects = function(object1, object2) {
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
+  
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+
+  for (const key of keys1) {
+    const value1 = object1[key];
+    const value2 = object2[key];
+
+    if (Array.isArray(value1) && Array.isArray(value2)) {
+      if (!eqArrays(value1, value2)) {
+        return false;
+      }
+    } else {
+      if (value1 !== value2) {
+        return false;
+      }
+    }
+  }
+  
+  return true;
+};
+
+
+
+// Test Cases
+const shirtObject = { color: "red", size: "medium" };
+const anotherShirtObject = { size: "medium", color: "red" };
+assertEqual(eqObjects(shirtObject, anotherShirtObject), true);
+
+const longSleeveShirtObject = { size: "medium", color: "red", sleeveLength: "long" };
+assertEqual(eqObjects(shirtObject, longSleeveShirtObject), false);
+
+
