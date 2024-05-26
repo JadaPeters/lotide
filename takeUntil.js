fa62dc14c18073f1a522378e69c8a7bc849fc340
Jada <jadapeters8@gmail.com>
2024-05-26 19:28:09 -0400

    takeUntil.js function created

---
+++ takeUntil.js
@@ -0,0 +1,45 @@
+// eqArrays Function 
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length){
+    return false
+  }
+  
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false; 
+    } 
+  }
+  return true;
+}
+
+// assertArraysEqual Function
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// takeUntil Function
+const takeUntil = function(array, callback) {
+  const result = [];
+  for (const item of array) {
+    if (callback(item)) {
+      break;
+    }
+    result.push(item);
+  }
+  return result;
+};
+
+// Test Cases
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
